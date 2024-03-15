0be8cb0551c0206e39d616a64104496433a62854
natalystx <nattapong.anuwong@gmail.com>
2024-03-15 17:42:15 +0700

    fix: time interval for each queue to be execute

--- src/utils/queue.ts
+++ src/utils/queue.ts
@@ -1,18 +1,22 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export default class Queue {
   private _queue: string[] = [];
-  private _tasks = new Map<string, { type: string; data: any }>();
+  private _tasks = new Map<string, { type: string; data: any; time: number }>();
   private _timeInterval: number;
   private _intervalRef: number | undefined = undefined;
   private _subscriber = new Set<(event: { type: string; data: any }) => void>();
+  private _baseInterval = 1000;
 
   constructor(timeInterval: number) {
     this._timeInterval = timeInterval;
   }
 
   add(id: string, event: { type: string; data: unknown }) {
+    this._tasks.set(id, {
+      ...event,
+      time: Date.now() + this._timeInterval,
+    });
     this._queue.push(id);
-    this._tasks.set(id, event);
 
     if (!this._intervalRef) {
       this._interval();
@@ -43,19 +47,22 @@ export default class Queue {
   private _interval = () => {
     if (!this._intervalRef) {
       this._intervalRef = setInterval(() => {
-        if (this._queue.length === 1) {
-          this._clearQueue();
-        }
-
-        this._subscriber.forEach((sub) => {
-          const payload = this._tasks.get(this._queue[0]);
-          if (payload) {
-            sub(payload);
+        const payload = this._tasks.get(this._queue[0]);
+        const diffTime = (payload?.time || 0) - Date.now();
+        if (diffTime <= 0) {
+          if (this._queue.length === 1) {
+            this._clearQueue();
           }
-        });
-        this._tasks.delete(this._queue[0]);
-        this._queue.shift();
-      }, this._timeInterval);
+
+          this._subscriber.forEach((sub) => {
+            if (payload) {
+              sub(payload);
+            }
+          });
+          this._tasks.delete(this._queue[0]);
+          this._queue.shift();
+        }
+      }, this._baseInterval);
     }
   };
 }
